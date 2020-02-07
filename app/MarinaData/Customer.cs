d60478971754cd9fdef14e8c96c295e8d8813d68
Harry Willis <harry.willis@edu.sait.ca>
2020-02-06 17:05:12 -0700

    get all customers and add new customer operations on db

--- app/MarinaData/Customer.cs
+++ app/MarinaData/Customer.cs
@@ -15,6 +15,7 @@ namespace MarinaData
         // auto-implemented properties
         public int ID { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Phone { get; set; }
         public string City { get; set; }
     }
