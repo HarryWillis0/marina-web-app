926029bb122dcf4cb2b713ea8f22e7f793340523
Harry Willis <harry.willis@edu.sait.ca>
2020-02-06 19:25:26 -0700

    registration / login / manage

--- app/app/Models/IdentityModels.cs
+++ app/app/Models/IdentityModels.cs
@@ -13,6 +13,7 @@ namespace app.Models
     // You can add User data for the user by adding more properties to your User class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        // extended properties
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
